db20a526b5109ee781bf905f9ddb483828873df6
iBeAnthony <43604022+iBeAnthony@users.noreply.github.com>
2018-11-09 01:50:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = ("$")
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
 const prefix = "."
 client.on('message', async msg => { 
 	if (msg.author.bot) return undefined;
